b24baa127887ee7ec16c80a018f8f551adcf9f1b
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-11-05 11:28:26 +0000

    Create requirejs-config.js

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            // Alias the legacy module ID to the core jQuery UI from lib/web
+            'Magento_Ui/js/lib/jquery/jquery-ui': 'jquery/jquery-ui'
+        }
+    }
+};
